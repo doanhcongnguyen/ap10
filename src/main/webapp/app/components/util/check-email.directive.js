558977ca31108eeb5aff979769e3b217442c65f6
DoanhNC <doanhcongnguyen@gmail.com>
2016-09-21 17:21:19 +0700

    Init html5mode

---
+++ src/main/webapp/app/components/util/check-email.directive.js
@@ -0,0 +1,26 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('appApp')
+        .directive('checkEmail', checkEmail);
+
+    function checkEmail() {
+        var EMAIL_REGEXP = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i;
+
+        var directive = {
+            require: '?ngModel',
+            link: function(scope, elm, attrs, ctrl) {
+                // only apply the validator if ngModel is present and Angular has added the email validator
+                if (ctrl && ctrl.$validators.email) {
+                    // this will overwrite the default Angular email validator
+                    ctrl.$validators.email = function(modelValue) {
+                      return ctrl.$isEmpty(modelValue) || EMAIL_REGEXP.test(modelValue);
+                    };
+                }
+            }
+        };
+
+        return directive;
+    }
+})();
---
+++ src/main/webapp/app/components/util/compare-to.directive.js
@@ -0,0 +1,34 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('appApp')
+        .directive('compareTo', compareTo);
+
+    function compareTo() {
+
+        var directive = {
+            require: "ngModel",
+            scope: {
+                otherModelValue: "=compareTo"
+            },
+            link: function(scope, element, attributes, ngModel) {
+
+                ngModel.$validators.compareTo = function(modelValue) {
+                    if (modelValue instanceof Object) {
+                        return angular.toJson(modelValue) == angular.toJson(scope.otherModelValue);
+                    } else {
+                        return modelValue == scope.otherModelValue;
+                    }
+                };
+
+                scope.$watch("otherModelValue", function() {
+                    ngModel.$validate();
+                });
+            }
+        };
+
+        return directive;
+    }
+
+})();
